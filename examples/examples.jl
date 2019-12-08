690e42e5375e6a6a83f4bb74f6a0109a61c0db88
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-12-08 16:41:46 +0100

    extended examples

--- examples/examples.jl
+++ examples/examples.jl
@@ -2,3 +2,10 @@ using HashDicts: HashDict
 
 d = HashDict([("A", 1), ("B", 2)])
 d2 = HashDict("A"=>1, "B"=>2)
+
+for (k, v) in [("hi", 2), ("", 0)]
+    global d
+    d[k] = v
+    @assert d[k] == v
+end
+ 
